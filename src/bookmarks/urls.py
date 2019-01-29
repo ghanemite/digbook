2dbf0f025edc478474946f5934ce46a030a0a4ee
G <ghanemite@users.noreply.github.com>
2019-01-29 13:09:10 +0200

    Basic Registration and simple ui

--- src/bookmarks/urls.py
+++ src/bookmarks/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
 
-from .views import main_page, user_page, logout_page
+from .views import main_page, user_page, logout_page, register_page
 
 urlpatterns = [
     url(r'^$', main_page, name='home'),
     url(r'^user/(\w+)/$', user_page, name='user_page'),
+    url(r'^signup/$', register_page, name='signup'),
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout_page, name='logout'),
 ]
--- src/bookmarks/views.py
+++ src/bookmarks/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import RedirectView
+
+from .forms import RegistrationForm
 
 
 def main_page(request):
@@ -24,3 +28,26 @@ def user_page(request, username):
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password2')
+            email    = form.cleaned_data.get('email')
+            user = User.objects.create_user(
+                username = username,
+                password = password,
+                email = email
+            )
+            messages.success(request, '''Your accounted created successfully\n
+            Pleaese Log in .''')
+            return HttpResponseRedirect('/')
+    else:
+        form = RegistrationForm()
+    return render(request,'registration/signup.html', {'form':form})
+
+
+    
